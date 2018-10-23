d8d0e87419745d67fcdbf669992d3af9769f0ff1
zw <zw9161@dingtalk.com>
2018-10-23 18:31:09 +0800

    用component is=来 替换组件更换

--- src/views/vue/core/VueCore.vue
+++ src/views/vue/core/VueCore.vue
@@ -4,13 +4,11 @@
        <Button v-for='(item,index) in qusArray' :key='index' :type='$GetType(index)'
       @click="openDrawer(index)">{{qusArray[index].title}}</Button>
     </div>
-    <Drawer :title="activeData.title||''"
+    <Drawer :title="qusArray[activeIndex].title||''"
       :closable="false"
       v-model="drawerShow"
       width="640">
-    <GlobalComponent v-if='activeData.title==="全局组件"'></GlobalComponent>
-    <LifeCycle v-if='activeData.title==="生命周期"'></LifeCycle>
-    <ComputedAttribute v-if='activeData.title==="计算属性"'></ComputedAttribute>
+    <component :is='activeComponentName'></component>
     </Drawer>
   </div>
 </template>
@@ -24,25 +22,27 @@ export default {
   },
   data () {
     return {
-      activeData: null,
+      activeComponentName: '',
+      activeIndex: 0,
       drawerShow: false,
       qusArray: [
-        {title: '全局组件', isShow: false},
-        {title: '生命周期', isShow: false},
-        {title: '计算属性', isShow: false}
+        {title: '全局组件', name: 'GlobalComponent'},
+        {title: '生命周期', name: 'LifeCycle'},
+        {title: '计算属性', name: 'ComputedAttribute'}
       ]
     }
   },
   created () {
-    this.activeData = this.qusArray[0] // 在create中null值就不会报错了。因为在挂载之前就进行赋值了
+    this.activeComponentName = this.qusArray[0].name
   },
   mounted () {
   },
   methods: {
     openDrawer (index) {
-      this.activeData = this.qusArray[index]
       this.drawerShow = !this.drawerShow
-      this.$set(this.qusArray[index], 'isShow', this.drawerShow)
+      this.activeIndex = index
+      this.activeComponentName = this.qusArray[index].name
+      console.log(this.activeIndex, this.activeComponentName)
     }
   }
 }
