c3f4aeb4dbfb87a6e09ae88ddf429a77c4126760
zw <zw9161@dingtalk.com>
2018-10-24 11:05:23 +0800

    对密码进行验证

--- src/api/mock.js
+++ src/api/mock.js
@@ -5,7 +5,7 @@ Mock.setup({
 })
 Mock.mock('http://192.168.21.87:8080/user/login', 'post', function (options) {
   const data = JSON.parse(options.body)
-  if (data.username === 'zuowang') {
+  if (data.username === 'zuowang' && data.password === '12345') {
     return {
       // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
       // 属性 id 是一个自增数，起始值为 1，每次增 1
